4533efc87b38a461da4b41fe0c56f7d8e61df238
nanikuper-prog <nanikuper@gmail.com>
2026-09-18 23:52:55 +0000

    Crear componente MapArea placeholder

---
+++ components/MapArea.tsx
@@ -0,0 +1,9 @@
+const MapArea = () => {
+  return (
+    <div className="flex h-64 items-center justify-center rounded-xl bg-gray-200 md:h-full">
+      <span className="font-medium text-gray-500">Mapa</span>
+    </div>
+  );
+};
+
+export default MapArea;
